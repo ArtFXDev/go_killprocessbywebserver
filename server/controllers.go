bd1f90578aa687cf81bb2d0339342df55f4de98c
OlivierArgentieri <olivierargentieri@gmail.com>
2022-03-10 12:34:25 +0100

    start list process

--- server/controllers.go
+++ server/controllers.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os/exec"
 	"strconv"
+	"encoding/json"
 
 	"github.com/OlivierArgentieri/go_killprocess/responses"
 	"github.com/gorilla/mux"
@@ -32,4 +33,43 @@ func (server *Server) KillProcess(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("Kill successfull")
 	responses.JSON(w, http.StatusOK, "Success")
+}
+
+func (server *Server) GetProcesses(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Received get processes request\n")
+
+	const cmd_powershell = `get-wmiObject Win32_PerfFormattedData_PerfProc_Process | ? {$_.Name -notlike '*_Total'} | ? {$_.Name -notlike '*Idle'} | ? {$_.PercentProcessorTime -gt '0'} | sort-object PercentProcessorTime -desc | select @{N='Name';E={$_.Name}}, @{N='CPU';E={$_.PercentProcessorTime}}, @{N='RAM';E={([math]::Round($_.WorkingSetPrivate/1Mb,2))}} | ConvertTo-Json`
+	const cmd_powershell_test = `write-output 'ddddddd'`
+
+	get_processes := exec.Command("powershell.exe", cmd_powershell)
+	log.Print(get_processes)
+
+	stdout, stderr := get_processes.CombinedOutput()
+
+	//err := get_processes.Run()
+
+	if stderr != nil {
+		responses.JSON(w, http.StatusOK, "Error when trying to get list of process.")
+		log.Printf("Error when trying to get list of process.")
+		log.Printf("err processes: ", stderr)
+		log.Printf("log processes: ", string(stdout))
+		return
+	}
+
+	type ProcessRow struct {
+		Name string
+		CPU float32
+		RAM float32
+	}
+
+	err := json.Unmarshal(stdout, ProcessRow)
+	if err != nil {
+		responses.JSON(w, http.StatusOK, "Error when trying to reformat stdout json.")
+		log.Printf("Error when trying to reformat stdout json.")
+		log.Printf("Error reformat stdout json: ", err)
+		return
+	}
+
+	log.Printf("Ok get processes: ", string(beautifulJsonByte))
+	responses.JSON(w, http.StatusOK, base64.StdEncoding.DecodeString(beautifulJsonByte))
 }
--- server/routes.go
+++ server/routes.go
@@ -5,5 +5,5 @@ import "github.com/OlivierArgentieri/go_killprocess/middlewares"
 func (server *Server) initRoutes() {
 	// route
 	server.Router.HandleFunc("/kill/{pid}", middlewares.SetMiddlewareJSON(server.KillProcess)).Methods("POST")
-
+	server.Router.HandleFunc("/processes", middlewares.SetMiddlewareJSON(server.GetProcesses)).Methods("GET")
 }

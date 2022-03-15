64a5e7ef0fa5bab4d675875cce1348f112e31762
OlivierArgentieri <olivierargentieri@gmail.com>
2022-03-15 11:43:15 +0100

    add cors middleware

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -10,3 +10,12 @@ func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
 		next(w, r)
 	}
 }
+
+func SetMiddlewareCORS(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		next(w, r)
+	}
+}
--- server/routes.go
+++ server/routes.go
@@ -4,6 +4,6 @@ import "github.com/OlivierArgentieri/go_killprocess/middlewares"
 
 func (server *Server) initRoutes() {
 	// route
-	server.Router.HandleFunc("/kill/{pid}", middlewares.SetMiddlewareJSON(server.KillProcess)).Methods("POST")
-	server.Router.HandleFunc("/processes", middlewares.SetMiddlewareJSON(server.GetProcesses)).Methods("GET")
+	server.Router.HandleFunc("/kill/{pid}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareCORS(server.KillProcess))).Methods("POST")
+	server.Router.HandleFunc("/processes", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareCORS(server.GetProcesses))).Methods("GET")
 }

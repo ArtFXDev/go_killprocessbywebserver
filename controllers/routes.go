6e7849f9e337109159c47c3c49afc8c3d1aef749
OlivierArgentieri <olivierargentieri@gmail.com>
2022-05-22 22:31:15 +0200

    remove nimby part

--- controllers/routes.go
+++ controllers/routes.go
@@ -8,8 +8,4 @@ func (server *Server) initRoutes() {
 	// Kill process
 	server.Router.HandleFunc("/kill/{pid}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareCORS(server.KillProcess))).Methods("POST")
 	server.Router.HandleFunc("/processes", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareCORS(server.GetProcesses))).Methods("GET")
-
-	// Nimby
-	server.Router.HandleFunc("/status", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareCORS(server.GetProcesses))).Methods("GET")
-	server.Router.HandleFunc("/status", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareCORS(server.GetProcesses))).Methods("POST")
 }
